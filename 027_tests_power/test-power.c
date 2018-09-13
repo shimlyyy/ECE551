7de03c94f08971443ccb10381004645d6ccf7c74
xl258 <xl258@duke.edu>
2018-09-13 11:30:52 -0400

    turn

--- 027_tests_power/test-power.c
+++ 027_tests_power/test-power.c
@@ -4,24 +4,15 @@ unsigned power(unsigned x, unsigned y);
 int run_check(unsigned x, unsigned y, unsigned expected_ans) {
   unsigned ans = power(x, y);
   if (ans == expected_ans) {
-    return 1;
+    return EXIT_SUCCESS;
   }
   else {
     exit(EXIT_FAILURE);
   }
 }
 int main(void) {
-  unsigned x = 4294967295;
-  unsigned y = 1;
-  int w, z, m;
-  w = run_check(4294967295, 1, 4294967295);
-  x = -1;
-  y = 2;
-  z = run_check(-1, 2, 1);
-  x = 0;
-  y = 0;
-  m = run_check(0, 0, 1);
-  if (z + w + m == 3) {
-    return EXIT_SUCCESS;
-  }
+  run_check(4294967295, 1, 4294967295);
+  run_check(-1, 2, 1);
+  run_check(0, 0, 1);
+  return EXIT_SUCCESS;
 }
