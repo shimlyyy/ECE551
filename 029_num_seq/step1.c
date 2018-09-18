08926c9a1cb8b89d853227e23de93199b96d26c0
xl258 <xl258@duke.edu>
2018-09-18 19:27:20 -0400

    submit

--- 029_num_seq/step1.c
+++ 029_num_seq/step1.c
@@ -20,19 +20,12 @@ void printSeq1Range(int low, int high) {
 }
 //  Step 1 (B): write main to test seq1
 int main(void) {
-
   printf("seq1(%d)=%d\n", -3, seq1(-3));
-
   printf("seq1(%d)=%d\n", -2, seq1(-2));
-
   printf("seq1(%d)=%d\n", -1, seq1(-1));
-  
   printf("seq1(%d)=%d\n", 0, seq1(0));
-  
   printf("seq1(%d)=%d\n", 1, seq1(1));
-
   printf("seq1(%d)=%d\n", 10, seq1(10));
-
   printf("seq1(%d)=%d\n", 11, seq1(11));
   printf("printSeq1Range(%d, %d)\n", 0, 0);
   printSeq1Range(0, 0);
