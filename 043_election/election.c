9723bba76f7b45302c907402e3903921ccbf5180
xl258 <xl258@duke.edu>
2018-09-28 21:55:07 -0400

    draft

--- 043_election/election.c
+++ 043_election/election.c
@@ -19,43 +19,44 @@ state_t parseLine(const char * line) {
   }
   state_t state;
   size_t i = 0;
-  while (*line != ':') {
+  while (line[i] != ':') {
     if (i == MAX_STATE_NAME_LENGTH) {
       fprintf(stderr, "name is too long. Invalid input");
       exit(EXIT_FAILURE);
     }
-    state.name[i] = *line;
+    state.name[i] = line[i];
     i++;
-    line++;
   }
   state.name[i] = '\0';
-  line++;
+  i++;
   state.population = 0;
-  while (*line != ':') {
-    state.population = state.population * 10 + (*line - '0');
-    line++;
+  while (line[i] != ':') {
+    state.population = state.population * 10 + (line[i] - '0');
+    i++;
   }
-  line++;
+  i++;
   state.electoralVotes = 0;
-  while (*line != '\0') {
-    state.electoralVotes = state.electoralVotes * 10 + (*line - '0');
-    line++;
+  while (line[i] != '\0') {
+    state.electoralVotes = state.electoralVotes * 10 + (line[i] - '0');
+    i++;
   }
   return state;
 }
 
 unsigned int countElectoralVotes(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 2: write me
-  uint64_t str_half;
+  size_t count = 0;
+  state_t * check = stateData;
+  while (check != NULL) {
+    count++;
+  }
+  if (count != nStates) {
+    fprintf(stderr, "input is invalid");
+    exit(EXIT_FAILURE);
+  }
   unsigned int totalvotes = 0;
   for (size_t i = 0; i < nStates; i++) {
-    if (stateData[i].population % 2 == 0) {
-      str_half = stateData[i].population / 2;
-    }
-    else {
-      str_half = stateData[i].population / 2 + 1;
-    }
-    if (voteCounts[i] > str_half) {
+    if (2 * voteCounts[i] > stateData[i].population) {
       totalvotes += stateData[i].electoralVotes;
     }
   }
@@ -64,8 +65,28 @@ unsigned int countElectoralVotes(state_t * stateData, uint64_t * voteCounts, siz
 
 void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 3: write me
+  for (size_t i = 0; i < nStates; i++) {
+    double t = voteCounts[i] / (double)stateData[i].population;
+    if (0.495 <= t && 0.505 >= t) {
+      printf("%s requires a recount(Candidate A has %.2f%% of the vote)\n",
+             stateData[i].name,
+             t * 100);
+    }
+  }
 }
 
 void printLargestWin(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 4: write me
+
+  size_t largestindex = 0;
+  double large = voteCounts[largestindex] / (double)stateData[largestindex].population;
+  double current;
+  for (size_t i = 1; i < nStates; i++) {
+    current = voteCounts[i] / (double)stateData[i].population;
+    if (current > large) {
+      large = current;
+      largestindex = i;
+    }
+  }
+  printf("Candidate A won %s with %.2f%% of the vote\n", stateData[largestindex].name, large * 100);
 }
