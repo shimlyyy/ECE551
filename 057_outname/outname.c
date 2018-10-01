51eed71e2963b209a727120a9166e7e864561c29
xl258 <xl258@duke.edu>
2018-10-01 16:03:02 -0400

    first

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,13 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * outFileName = malloc((strlen(inputName) + 8) * sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+  return outFileName;
 }
