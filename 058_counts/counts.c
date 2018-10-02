4e90b47e8f511ce78e0b32e0297295c8591ed5f6
xl258 <xl258@duke.edu>
2018-10-01 21:47:30 -0400

    first

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,44 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * ans = malloc(sizeof(*ans));
+  ans->numValue = 0;
+  ans->valueName = NULL;
+  return ans;
 }
+
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  c->unknown = 0;
+  if (name == NULL) {
+    c->unknown++;
+  }
+  for (int i = 0; i < c->numValue; i++) {
+    if (strcmp(c->valueName[i]->str, name) == 0) {
+      c->valueName[i]->count++;
+    }
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for (int i = 0; i < c->numValue; i++) {
+    printf("%s: %d\n", c->valueName[i]->str, c->valueName[1]->count);
+  }
+  if (c->unknown != 0) {
+    printf("<unknown> : %d\n", c->unknown);
+  }
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for (int i = 0; i < c->numValue; i++) {
+    free(c->valueName[i]->str);
+    free(c->valueName[i]);
+  }
+  free(c->valueName);
+  free(c);
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,18 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
+#include <stdio.h>
 struct _one_count_t {
   //DEFINE ME
-
+  char * str;
+  int count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** valueName;
+  int numValue;
+  int unknown;
 };
 typedef struct _counts_t counts_t;
 
