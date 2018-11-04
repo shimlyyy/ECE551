a919f36501d0c2e7f938528f83f6c568f89798a2
xl258 <xl258@duke.edu>
2018-11-04 00:52:47 -0400

    turn

--- 094_binsrch/function.h
+++ 094_binsrch/function.h
@@ -1,9 +1,29 @@
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
-template<typename R, typename A> 
-class Function {
-public:
+template<typename R, typename A>
+class Function
+{
+ public:
   virtual R invoke(A arg) = 0;
   virtual ~Function() {}
 };
+
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  if (f->invoke(high) < 0) {
+    return high - 1;
+  }
+  if (f->invoke(low) > 0) {
+    return low;
+  }
+  int mid = low + (high - low) / 2;
+  if (f->invoke(mid) == 0) {
+    return mid;
+  }
+  else if (f->invoke(mid) > 0) {
+    return binarySearchForZero(f, low, mid);
+  }
+  else {
+    return binarySearchForZero(f, mid, low);
+  }
+}
 #endif
---
+++ 094_binsrch/test.cpp
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdio.h>
+
+#include <cstdlib>
+
+#include "function.h"
+//int binarySearchForZero(Function<int, int> * f, int low, int high);
+class linearFunction : public Function<int, int>
+{
+ public:
+  virtual int invoke(int arg) { return 2 * arg + 10; }
+};
+class CountedIntFn : public Function<int, int>
+{
+ protected:
+  unsigned remaining;
+  Function<int, int> * f;
+  const char * mesg;
+
+ public:
+  CountedIntFn(unsigned n, Function<int, int> * fn, const char * m) :
+      remaining(n),
+      f(fn),
+      mesg(m) {}
+  virtual int invoke(int arg) {
+    if (remaining == 0) {
+      fprintf(stderr, "Too many function invocations in %s\n", mesg);
+      exit(EXIT_FAILURE);
+    }
+    remaining--;
+    return f->invoke(arg);
+  }
+};
+void check(Function<int, int> * f, int low, int high, int expected_ans, const char * mesg) {
+  int ans = binarySearchForZero(f, low, high);
+  if (ans == expected_ans) {
+    printf("function %s between %d and %d success\n", mesg, low, high);
+  }
+  else {
+    printf("fucntion %s between %d and %d :answer is not equal to expected ans\n", mesg, low, high);
+    exit(EXIT_FAILURE);
+  }
+}
+
+class powerFunction : public Function<int, int>
+{
+ public:
+  virtual int invoke(int arg) { return arg * (arg - 2); }
+};
+
+int main(void) {
+  Function<int, int> * f2 = new linearFunction;
+  const char * mesg2 = "2*arg+10";
+  Function<int, int> * c = new CountedIntFn(8, f2, mesg2);
+  check(c, -138, -10, -11, mesg2);
+
+  Function<int, int> * f3 = new powerFunction;
+  const char * mesg3 = "arg*(arg-2)";
+  Function<int, int> * d = new CountedIntFn(8, f3, mesg3);
+  check(d, 0, 128, 2, mesg3);
+  Function<int, int> * d1 = new CountedIntFn(8, f3, mesg3);
+  check(d1, 2, 130, 2, mesg3);
+
+  return EXIT_SUCCESS;
+}
