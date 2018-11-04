34f7dd9effec84f0171eccbaf7ef9f60d09b33ff
xl258 <xl258@duke.edu>
2018-11-04 14:20:35 -0500

    turn

--- 094_binsrch/function.h
+++ 094_binsrch/function.h
@@ -10,26 +10,17 @@ class Function
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
   int mid = low + (high - low) / 2;
+  if (high - low == 1) {
+    return high - 1;
+  }
   int result = f->invoke(mid);
-  if (mid == low) {
-    return low;
-  }
-  if (mid == high) {
-    return mid - 1;
-  }
   if (result == 0) {
     return mid;
   }
   else if (result > 0) {
-    /* if (mid == low + 1) {
-      return low;
-      }*/
-    return binarySearchForZero(f, low, mid - 1);
+    return binarySearchForZero(f, low, mid);
   }
   else {
-    /* if (mid == high - 1) {
-      return mid;
-      }*/
     return binarySearchForZero(f, mid + 1, high);
   }
 }
