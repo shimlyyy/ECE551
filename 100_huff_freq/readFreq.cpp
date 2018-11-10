a6c3ba1141e3587ba5d986f8fa54b68fb3bcce76
xl258 <xl258@duke.edu>
2018-11-10 15:09:37 -0500

    turn

--- 100_huff_freq/readFreq.cpp
+++ 100_huff_freq/readFreq.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +23,22 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  uint64_t * array = new uint64_t[257];
+  for (uint64_t i = 0; i < 257; i++) {
+    array[i] = 0;
+  }
+  int c;
+  std::ifstream input(fname);
+  if (input) {
+    while ((c = input.get()) != EOF) {
+      array[c]++;
+    }
+    array[256] = 1;
+    input.close();
+  }
+  else {
+    std::cerr << "can't open file\n";
+    exit(EXIT_FAILURE);
+  }
+  return array;
 }
