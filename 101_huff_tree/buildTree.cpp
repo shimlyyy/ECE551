d6c9ed561950135acd264f33b42a2b72c163305e
xl258 <xl258@duke.edu>
2018-11-11 14:31:38 -0500

    turn

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (uint64_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node * l = pq.top();
+    pq.pop();
+    Node * r = pq.top();
+    pq.pop();
+    Node * sum = new Node(l, r);
+    pq.push(sum);
+  }
+  return pq.top();
+}
